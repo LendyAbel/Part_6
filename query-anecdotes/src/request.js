a628eb242ad3021e3c12abb005fcaca401437c2b
LendyAbel <lendyabel93@gmail.com>
2025-07-24 13:11:26 +0200

    Exercise 6.21 Query

--- query-anecdotes/src/request.js
+++ query-anecdotes/src/request.js
@@ -6,3 +6,8 @@ export const getAll = async () => {
     const result = await axios.get(baseUrl)
     return result.data
 }
+
+export const createNew = async newAnecdote => {
+    const result = await axios.post(baseUrl, newAnecdote)
+    return result.data
+}
